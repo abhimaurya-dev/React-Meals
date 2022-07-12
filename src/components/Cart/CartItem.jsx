68027737caef90a6bb531be4eb247c17934c9624
abhimaurya-dev <abhishekmaurya.official@gmail.com>
2022-07-12 13:53:00 +0530

    Added Logic to components and managed states

---
+++ src/components/Cart/CartItem.jsx
@@ -0,0 +1,27 @@
+import styles from "./CartItem.module.css";
+
+const CartItem = ({ name, price, amount, onAdd, onRemove }) => {
+  const formattedPrice = `$${price.toFixed(2)}`;
+
+  return (
+    <li className={styles["cart-item"]}>
+      <div>
+        <h2>{name}</h2>
+        <div className={styles.summary}>
+          <span className={styles.price}>{formattedPrice}</span>
+          <span className={styles.amount}>x {amount}</span>
+        </div>
+      </div>
+      <div className={styles.actions}>
+        <button type="button" onClick={onRemove}>
+          −
+        </button>
+        <button type="button" onClick={onAdd}>
+          +
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
